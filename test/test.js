0f689c6956a4c3fd7efcb99afa9b40d73c44863e
Vitaly Yastremsky <vitaly@droicelabs.com>
2021-05-22 15:01:48 +0300

    upd

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require("assert");
 
-describe("Array", function () {
+describe("Arraffy", function () {
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
       assert.equal([1, 2, 3].indexOf(3), -1);
