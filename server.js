40c617a995398a128d37ed34e4bf5ec729989067
Vitaly Yastremsky <vitaly@droicelabs.com>
2021-05-22 14:30:51 +0300

    first

---
+++ server.js
@@ -0,0 +1,20 @@
+const ronin = require("ronin-server");
+const mocks = require("ronin-mocks");
+const database = require("ronin-database");
+const server = ronin.server();
+
+database.connect(process.env.CONNECTIONSTRING);
+server.use("/foo", (req, res) => {
+  return res.json({ foo: "bar" });
+});
+
+server.use("/", mocks.server(server.Router(), false, false));
+server.start();
+
+// const ronin = require("ronin-server");
+// const mocks = require("ronin-mocks");
+
+// const server = ronin.server();
+
+// server.use("/", mocks.server(server.Router(), false, true));
+// server.start();
